d8463b2984f8094657f1163db2b9a5f5f9d2d334
sinosu <sinosu@163.com>
2025-01-10 20:09:53 +0800

    change 2 deepseek

--- src/components/cards/aboutcard.tsx
+++ src/components/cards/aboutcard.tsx
@@ -12,12 +12,12 @@ export default function AboutCard() {
     <div className="max-w-xl mx-auto mt-10">
       <Card>
         <CardHeader>
-          <CardTitle>AKASH AI SDK Lite</CardTitle>
+          <CardTitle>Deepseek AI SDK Lite</CardTitle>
           <CardDescription>A no bells or whistles AI starter kit</CardDescription>
         </CardHeader>
         <CardContent className="text-sm text-muted-foreground/90 leading-normal prose"> 
-          <p className="mb-3">A simplified Akash AI starter kit designed with simplicity and speed in mind.</p>
-          <p className="mb-3">Built with Next.js, Akash api, Tailwind, Typescript and shadcn you can build a bare minimum AI Chatbot with only an environment variable. Based off the popular <Link href="https://chat.vercel.ai/">Next AI Chatbot</Link> the aim for this project is to remove any dependency outside of basic functionality and examples with an emphasis on making changes and experimenting with the AI SDK. </p>
+          <p className="mb-3">A simplified Deepseek AI starter kit designed with simplicity and speed in mind.</p>
+          <p className="mb-3">Built with Next.js, Deepseek api, Tailwind, Typescript and shadcn you can build a bare minimum AI Chatbot with only an environment variable. Based off the popular <Link href="https://chat.vercel.ai/">Next AI Chatbot</Link> the aim for this project is to remove any dependency outside of basic functionality and examples with an emphasis on making changes and experimenting with the AI SDK. </p>
         </CardContent>
       </Card>
     </div>
