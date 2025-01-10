269828451bb4aba6a47ffae3dfc3e1fbd5ed05a1
sinosu <sinosu@163.com>
2025-01-10 18:14:16 +0800

    for deepseek

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -3,14 +3,14 @@
 import OpenAI from 'openai';
 
 const client = new OpenAI({
-    apiKey: process.env.AKASH_API_KEY,
-    baseURL: "https://chatapi.akash.network/api/v1"
+    apiKey: process.env.DEEPSEEK_API_KEY,
+    baseURL: "https://api.deepseek.com"
   });
 
 export async function continueTextConversation(input: string) {
   const chatCompletion = await client.chat.completions.create({
     messages: [{ role: 'user', content: input }],
-    model: 'Meta-Llama-3-1-8B-Instruct-FP8',
+    model: 'deepseek-chat',
   });
 
   const result = await chatCompletion.choices[0].message.content;
